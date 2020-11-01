24da459f0ccd2df2dbd6d6d2e11d087d70c737cc
Bence Répási <60817330+Repakiraly@users.noreply.github.com>
2020-11-01 21:47:52 +0100

    Chess Engine in C - 19

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "defs.h"
+
+#define FEN1 "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
+#define FEN2 "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
+#define FEN3 "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
+
+int main() {
+
+    printf("My name is CJ Vercetti\nNice to meet you friend!\n");
+    AllInit();
+
+    S_BOARD board[1];
+	return 0;
+}
